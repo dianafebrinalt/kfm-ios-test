2199dd550e12900b4fb40dd055a78205218238f4
dianafebrinalt <febrinalt123@gmail.com>
2021-11-20 17:53:57 +0700

    Create Current Weather Model

---
+++ WeatherApp/Model/CurrentWeatherModel.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeatherModel.swift
+//  WeatherApp
+//
+//  Created by Diana Febrina Lumbantoruan on 20/11/21.
+//
+
+import Foundation
+
+struct CurrentWeather: Decodable {
+    let name: String?
+    let temp: Double?
+    let dt: Date?
+    let weather: [Weather]?
+}
+
+struct Weather: Decodable {
+    let main: String?
+    let icon: String?
+}
